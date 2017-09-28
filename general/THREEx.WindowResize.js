9f10811a027b177c7ac92fc109571e8537e59ad7
Denis Rostolopa <d.rostolopa@gmail.com>
2017-09-28 21:15:01 +0300

    Update THREEx.WindowResize.js

--- general/THREEx.WindowResize.js
+++ general/THREEx.WindowResize.js
@@ -1,50 +1,23 @@
-// This THREEx helper makes it easy to handle window resize.
-// It will update renderer and camera when window is resized.
-//
-// # Usage
-//
-// **Step 1**: Start updating renderer and camera
-//
-// ```var windowResize = THREEx.WindowResize(aRenderer, aCamera)```
-//    
-// **Step 2**: Start updating renderer and camera
-//
-// ```windowResize.stop()```
-// # Code
-
-//
-
-/** @namespace */
-var THREEx	= THREEx 		|| {};
-
-/**
- * Update renderer and camera when the window is resized
- * 
- * @param {Object} renderer the renderer to update
- * @param {Object} Camera the camera to update
-*/
-THREEx.WindowResize	= function(renderer, camera){
-	var callback	= function(){
-		// notify the renderer of the size change
-		
-		//renderer.setSize( window.innerWidth, window.innerHeight );
-		renderer.setSize( window.innerWidth, window.screen.availHeight);
-				
-		// update the camera
-		//camera.aspect	= window.innerWidth / window.innerHeight;
-		camera.aspect	= window.innerWidth / window.screen.availHeight;
-
-		camera.updateProjectionMatrix();
-	}
-	// bind the resize event
-	window.addEventListener('resize', callback, false);
-	// return .stop() the function to stop watching window resize
-	return {
-		/**
-		 * Stop watching window resize
-		*/
-		stop	: function(){
-			window.removeEventListener('resize', callback);
-		}
-	};
+var THREEx = THREEx || {};
+THREEx.WindowResize = function(renderer, camera) {
+    var callback = function() {
+        // notify the renderer of the size change
+        var ua = navigator.userAgent;
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|mobile|CriOS/i.test(ua)) {
+            renderer.setSize(window.innerWidth, window.screen.availHeight);
+            camera.aspect = window.innerWidth / window.screen.availHeight;
+        } else {
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            camera.aspect = window.innerWidth / window.innerHeight;
+        }
+        camera.updateProjectionMatrix();
+    }
+    // bind the resize event
+    window.addEventListener('resize', callback, false);
+    // return .stop() the function to stop watching window resize
+    return {
+        stop: function() {
+            window.removeEventListener('resize', callback);
+        }
+    };
 }
